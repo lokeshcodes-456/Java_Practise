aa634ec4a757f61004895a02b90b58e9a53cc06f
lokeshcodes-456 <lokeshsettypalli@gmail.com>
2025-07-30 16:14:41 +0530

    Create inputNum.java

---
+++ Basics/inputNum.java
@@ -0,0 +1,12 @@
+package Basics;
+import java.util.*;
+
+public class inputNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of num:");
+        int num=sc.nextInt();
+        System.out.println("Value of num is: "+num);
+        sc.close();
+    }
+}
